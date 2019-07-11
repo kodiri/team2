73ad7db1bb7ca3e26852b05da4a2d94995b6b5f6
Paraic Cloherty <p.cloherty@hotmail.co.uk>
2019-07-11 16:35:59 +0100

    css page done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './pages/home/home.js';
 import JavaScript from './pages/javascript/javascript';
 import ReactPage from './pages/react/react.js';
+import CSS from './pages/css/css.js';
 
 class App extends React.Component {
 
@@ -15,6 +16,7 @@ class App extends React.Component {
               <Route exact path='/' component={Home} />
               <Route exact path='/javascript' component={JavaScript} />
               <Route exact path='/react' component={ReactPage} />
+              <Route exact path='/css' component={CSS} />
             </div>
         </Router>
       </>
